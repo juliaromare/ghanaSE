2aef31a81ad20576829bcc700dd4d12028d913c5
julia.romare <julia.romare@gmail.com>
2017-01-23 21:58:28 +0000

    Add functionality

---
+++ Calc.java
@@ -0,0 +1,14 @@
+/**
+ * Created by juliaromare on 2017-01-20.
+ */
+public class Calc {
+
+        public static void main(String[] parameters) {
+            if (parameters[0].equals("add")) {
+                double one = Double.parseDouble(parameters[1]);
+                double two = Double.parseDouble(parameters[2]);
+                System.out.println(one + two);
+            }
+        }
+
+}
