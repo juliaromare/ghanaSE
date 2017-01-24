f6899aecc15a0b5d82598d465adb2a2e75581931
julia.romare <julia.romare@gmail.com>
2017-01-24 09:07:25 +0000

    subtract

--- Calc.java
+++ Calc.java
@@ -9,8 +9,14 @@ public class Calc {
                 double two = Double.parseDouble(parameters[2]);
                 System.out.println(one + two);
             }
+            else if(parameters[0].equals("subtract")){
+                double one = Double.parseDouble(parameters[1]);
+                double two = Double.parseDouble(parameters[2]);
+                System.out.println(one - two);
+
+            }
         }
 
-    
+
 
 }
